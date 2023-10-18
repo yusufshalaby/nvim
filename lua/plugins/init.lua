70348cd7c48aeb192aaa0db9c98ed3d244175c2a
Yusuf Shalaby <yusufshalaby@FT-MAC-YusufS.local>
2023-10-18 11:16:04 -0400

    nvim autopairs

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -62,6 +62,21 @@ return {
 			"hrsh7th/cmp-path",
 			-- Adds a number of user-friendly snippets
 			"rafamadriz/friendly-snippets",
+			-- autopairing of (){}[] etc
+			{
+				"windwp/nvim-autopairs",
+				opts = {
+					fast_wrap = {},
+					disable_filetype = { "TelescopePrompt", "vim" },
+				},
+				config = function(_, opts)
+					require("nvim-autopairs").setup(opts)
+
+					-- setup cmp for autopairs
+					local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+					require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+				end,
+			},
 		},
 		config = require("plugins.nvim-cmp"),
 	},
