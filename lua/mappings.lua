7e5fdef261b054fcffa6a7a73b86b5f96bb01873
Yusuf Shalaby <yusufshalaby@FT-MAC-YusufS.local>
2023-10-18 10:33:11 -0400

    telescope lazy load

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -32,22 +32,15 @@ map('n', '<leader>x', '<Cmd>BufferClose<CR>', opts)
 map('n', '<C-n>', '<Cmd>NvimTreeToggle<CR>', opts)
 
 -- telescope
-vim.keymap.set('n', '<leader>fo', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader>fx', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>fz', function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
-end, { desc = '[/] Fuzzily search in current buffer' })
-
-vim.keymap.set('n', '<leader>fg', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>fw', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>fr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
-vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>fo', "<CMD> Telescope oldfiles <CR>", { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader>fx', "<CMD> Telescope buffers <CR>", { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>fz', "<CMD> Telescope current_buffer_fuzzy_find <CR>", { desc = 'Fuzzily search in current buffer' })
+vim.keymap.set('n', '<leader>fg', "<CMD> Telescope git_files <CR>", { desc = 'Search [G]it [F]iles' })
+vim.keymap.set('n', '<leader>ff', "<CMD> Telescope find_files <CR>", { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>fw', "<CMD> Telescope live_grep <CR>", { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>fd', "<CMD> Telescope diagnostics <CR>", { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', '<leader>fr', "<CMD> Telescope resume <CR>", { desc = '[S]earch [R]esume' })
+vim.keymap.set('n', '<leader>fh', "<CMD> Telescope help_tags <CR>", { desc = '[S]earch [H]elp' })
 
 -- Comment
 vim.keymap.set('n', '<leader>/', function()
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,28 @@
 return function()
-  local telescope = require "telescope"
-  telescope.setup({
-    defaults = {
-      mappings = {
-        i = {
-          ['<C-u>'] = false,
-          ['<C-d>'] = false,
-        },
-      },
-    },
-  })
-  pcall(require('telescope').load_extension, 'fzf')
+	local telescope = require("telescope")
+	telescope.setup({
+		defaults = {
+			mappings = {
+				i = {
+					["<C-u>"] = false,
+					["<C-d>"] = false,
+				},
+			},
+			layout_strategy = "horizontal",
+			layout_config = {
+				horizontal = {
+					prompt_position = "bottom",
+					preview_width = 0.55,
+					results_width = 0.8,
+				},
+				vertical = {
+					mirror = false,
+				},
+				width = 0.87,
+				height = 0.80,
+				preview_cutoff = 120,
+			},
+		},
+	})
+	pcall(require("telescope").load_extension, "fzf")
 end
