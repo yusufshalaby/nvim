3037503ba4a63a47211cf63055a6227ef7c14dce
yusufshalaby <yusufshalaby@gmail.com>
2023-10-22 00:53:59 -0400

    added lua snippets to cmp

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -66,6 +66,7 @@ return {
 			"L3MON4D3/LuaSnip",
 			"saadparwaiz1/cmp_luasnip",
 			-- Adds LSP completion capabilities
+			"hrsh7th/cmp-nvim-lua",
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-path",
