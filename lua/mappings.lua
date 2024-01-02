abb0a542001a64afc547e397869c3e663f94b886
yusufshalaby <yusufshalaby@gmail.com>
2024-01-02 06:47:19 -0500

    configure trouble lazy loading

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -52,7 +52,7 @@ end, { desc = "Comment line" })
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
 vim.keymap.set("n", "<leader>fl", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+vim.keymap.set("n", "<leader>q", "<CMD> TroubleToggle workspace_diagnostics <CR>", { desc = "Open diagnostics list" })
 
 -- Treesitter Context
 vim.keymap.set("n", "<leader>tc", "<CMD>TSContextEnable<CR>", { desc = "Enable treesitter context" })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -223,6 +223,7 @@ return {
 	{
 		"folke/trouble.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
+		cmd = { "Trouble", "TroubleToggle", "TroubleRefresh" },
 		opts = {},
 	},
 	{
