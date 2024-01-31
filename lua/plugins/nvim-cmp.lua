5bf90c88f6024429db35d83bbf2d5d81405c796c
yusufshalaby <yusufshalaby@gmail.com>
2024-01-30 23:52:25 -0500

    clean nvim-cmp config

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,4 +1,34 @@
-return function()
+local M = {}
+
+M.dependencies = {
+	-- Snippet Engine & its associated nvim-cmp source
+	"L3MON4D3/LuaSnip",
+	"saadparwaiz1/cmp_luasnip",
+	-- Adds LSP completion capabilities
+	"hrsh7th/cmp-nvim-lua",
+	"hrsh7th/cmp-nvim-lsp",
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/cmp-path",
+	-- Adds a number of user-friendly snippets
+	"rafamadriz/friendly-snippets",
+	-- autopairing of (){}[] etc
+	{
+		"windwp/nvim-autopairs",
+		opts = {
+			fast_wrap = {},
+			disable_filetype = { "TelescopePrompt", "vim" },
+		},
+		config = function(_, opts)
+			require("nvim-autopairs").setup(opts)
+
+			-- setup cmp for autopairs
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		end,
+	},
+}
+
+M.config = function()
 	local cmp = require("cmp")
 	local luasnip = require("luasnip")
 	require("luasnip.loaders.from_vscode").lazy_load()
@@ -45,7 +75,9 @@ return function()
 			{ name = "luasnip" },
 			{ name = "path" },
 			{ name = "buffer" },
-			{ name = "nvim_lua" }
+			{ name = "nvim_lua" },
 		},
 	})
 end
+
+return M
