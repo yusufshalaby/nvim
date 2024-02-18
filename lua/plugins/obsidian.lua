5f10563e5333b8ef93779876acfdba5bcfd2ee6a
yusufshalaby <yusufshalaby@gmail.com>
2024-02-18 00:30:05 -0500

    fix obsidian deprecated settings

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -63,30 +63,22 @@ return {
 
 		-- Trigger completion at 2 chars.
 		min_chars = 2,
-
-		-- Where to put new notes created from completion. Valid options are
-		--  * "current_dir" - put new notes in same directory as the current buffer.
-		--  * "notes_subdir" - put new notes in the default notes subdirectory.
-		new_notes_location = "current_dir",
-
-		-- Either 'wiki' or 'markdown'.
-		preferred_link_style = "wiki",
-
-		-- Control how wiki links are completed with these (mutually exclusive) options:
-		--
-		-- 1. Whether to add the note ID during completion.
-		-- E.g. "[[Foo" completes to "[[foo|Foo]]" assuming "foo" is the ID of the note.
-		-- Mutually exclusive with 'prepend_note_path' and 'use_path_only'.
-		prepend_note_id = true,
-		-- 2. Whether to add the note path during completion.
-		-- E.g. "[[Foo" completes to "[[notes/foo|Foo]]" assuming "notes/foo.md" is the path of the note.
-		-- Mutually exclusive with 'prepend_note_id' and 'use_path_only'.
-		prepend_note_path = false,
-		-- 3. Whether to only use paths during completion.
-		-- E.g. "[[Foo" completes to "[[notes/foo]]" assuming "notes/foo.md" is the path of the note.
-		-- Mutually exclusive with 'prepend_note_id' and 'prepend_note_path'.
-		use_path_only = false,
 	},
+	-- Where to put new notes created from completion. Valid options are
+	--  * "current_dir" - put new notes in same directory as the current buffer.
+	--  * "notes_subdir" - put new notes in the default notes subdirectory.
+	new_notes_location = "current_dir",
+	-- Either 'wiki' or 'markdown'.
+	preferred_link_style = "wiki",
+	wiki_link_func = function(opts)
+		if opts.id == nil then
+			return string.format("[[%s]]", opts.label)
+		elseif opts.label ~= opts.id then
+			return string.format("[[%s|%s]]", opts.id, opts.label)
+		else
+			return string.format("[[%s]]", opts.id)
+		end
+	end,
 
 	-- Optional, configure key mappings. These are the defaults. If you don't want to set any keymappings this
 	-- way then set 'mappings = {}'.
