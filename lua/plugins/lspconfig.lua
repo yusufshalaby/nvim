a413709101ff42a2f97fe1328adfb55fcb957f61
yusufshalaby <yusufshalaby@gmail.com>
2024-02-19 19:20:53 -0500

    add marksman lsp

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -86,6 +86,7 @@ local servers = {
 	ruff_lsp = {},
 	lua_ls = {}, -- TJ Devries lua lsp setup: https://github.com/neovim/neovim/issues/21686#issuecomment-1522446128
 	taplo = {},
+	marksman = {},
 }
 
 -- Setup neovim lua configuration
