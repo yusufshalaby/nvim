b94f5b58119014ef2d3305ce13b3a87a76a789f9
yusufshalaby <yusufshalaby@gmail.com>
2024-03-09 20:38:37 -0500

    add lsp signature binding to insert mode

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -29,7 +29,8 @@ local on_attach = function(_, bufnr)
 
 	-- See `:help K` for why this keymap
 	nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-	nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+	-- nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+	vim.keymap.set({"n", "i"}, "<C-k>", vim.lsp.buf.signature_help, { desc = "Signature Documentation" })
 
 	-- Lesser used LSP functionality
 	nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
