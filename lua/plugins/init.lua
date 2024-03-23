c2637ece9ee45a66d9ef049e475fd95913897eaa
yusufshalaby <yusufshalaby@gmail.com>
2024-03-23 14:22:42 -0400

    add gx functionality without netrw

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -355,4 +355,14 @@ return {
 		end,
 		ft = "csv",
 	},
+	{
+		"chrishrb/gx.nvim",
+		keys = { { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } } },
+		cmd = { "Browse" },
+		init = function()
+			vim.g.netrw_nogx = 1 -- disable netrw gx
+		end,
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = true,
+	},
 }
