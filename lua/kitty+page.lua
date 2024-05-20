9dedf8f5dbe0c72de387c0e2a42cb99b47b1c81d
yusufshalaby <yusufshalaby@gmail.com>
2024-05-20 17:43:52 -0400

    (hopefully) fixed the issue where the cursor is being placed too early

--- lua/kitty+page.lua
+++ lua/kitty+page.lua
@@ -1,8 +1,6 @@
 -- https://github.com/kovidgoyal/kitty/issues/719#issuecomment-1721699211
-local setup = function()
-	local nlines = vim.fn.getenv("INPUT_LINE_NUMBER")
-	local cur_line = vim.fn.getenv("CURSOR_LINE")
-	local cur_column = vim.fn.getenv("CURSOR_COLUMN")
+return function(INPUT_LINE_NUMBER, CURSOR_LINE, CURSOR_COLUMN)
+	-- print("INPUT_LINE_NUMBER:", INPUT_LINE_NUMBER, "CURSOR_LINE:", CURSOR_LINE, "CURSOR_COLUMN:", CURSOR_COLUMN)
 	vim.opt.encoding = "utf-8"
 	vim.opt.clipboard = "unnamed"
 	vim.opt.compatible = false
@@ -19,10 +17,39 @@ local setup = function()
 	-- use the visual color matching the current colorscheme
 	vim.cmd("hi Visual ctermbg=52 guibg=#543a48")
 	vim.keymap.set("n", "<Esc>", "<Cmd>noh<CR>")
+	vim.keymap.set("n", "<C-d>", "<C-d>zz")
+	vim.keymap.set("n", "<C-u>", "<C-u>zz")
+	vim.keymap.set("n", "n", "nzzzv")
+	vim.keymap.set("n", "N", "Nzzzv")
 	local term_buf = vim.api.nvim_create_buf(true, false)
 	local term_io = vim.api.nvim_open_term(term_buf, {})
 	vim.api.nvim_buf_set_keymap(term_buf, "n", "q", "<Cmd>q<CR>", {})
 	local group = vim.api.nvim_create_augroup("kitty+page", {})
+	local vimEnterCompleted = false
+
+	local setCursor = function()
+		vim.api.nvim_feedkeys(tostring(INPUT_LINE_NUMBER) .. [[ggzt]], "n", true)
+		local line = vim.api.nvim_buf_line_count(term_buf)
+		if CURSOR_LINE <= line then
+			line = CURSOR_LINE
+		end
+		line = CURSOR_LINE
+		vim.api.nvim_feedkeys(tostring(line - 1) .. [[j]], "n", true)
+		vim.api.nvim_feedkeys([[0]], "n", true)
+		vim.api.nvim_feedkeys(tostring(CURSOR_COLUMN - 1) .. [[l]], "n", true)
+	end
+
+	vim.api.nvim_create_autocmd("ModeChanged", {
+		group = group,
+		buffer = term_buf,
+		callback = function()
+			local mode = vim.fn.mode()
+			if mode == "t" then
+				vim.cmd.stopinsert()
+				vim.schedule(setCursor)
+			end
+		end,
+	})
 
 	vim.api.nvim_create_autocmd("VimEnter", {
 		group = group,
@@ -30,27 +57,19 @@ local setup = function()
 		once = true,
 		callback = function(ev)
 			local current_win = vim.fn.win_getid()
-			for _, line in ipairs(vim.api.nvim_buf_get_lines(ev.buf, 0, -1, false)) do
+			for _, line in ipairs(vim.api.nvim_buf_get_lines(ev.buf, 0, -2, false)) do
 				vim.api.nvim_chan_send(term_io, line)
 				vim.api.nvim_chan_send(term_io, "\r\n")
 			end
-			-- term_io = false
-			vim.api.nvim_create_autocmd("ModeChanged", {
-				group = group,
-				pattern = "([nN]:[^vV])|([vV]:[^nN])",
-				command = "stopinsert",
-			})
-			vim.api.nvim_win_set_buf(current_win, term_buf)
-			if nlines ~= vim.NIL and cur_line ~= vim.NIL and cur_column ~= vim.NIL then
-				vim.api.nvim_win_set_cursor(current_win, { vim.fn.max({ 1, nlines }) + cur_line, cur_column - 1 })
-			else
-				vim.api.nvim_input([[<C-\><C-n>G]])
+			for _, line in ipairs(vim.api.nvim_buf_get_lines(ev.buf, -2, -1, false)) do
+				vim.api.nvim_chan_send(term_io, line)
 			end
+			vim.api.nvim_win_set_buf(current_win, term_buf)
 			vim.api.nvim_buf_delete(ev.buf, { force = true })
+			vimEnterCompleted = true
 		end,
 	})
-	-- [[ Highlight on yank ]]
-	-- See `:help vim.highlight.on_yank()`
+
 	local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 	vim.api.nvim_create_autocmd("TextYankPost", {
 		callback = function()
@@ -59,5 +78,14 @@ local setup = function()
 		group = highlight_group,
 		pattern = "*",
 	})
+
+	local function afterVimEnter()
+		if vimEnterCompleted then
+			vim.schedule(setCursor)
+		else
+			vim.defer_fn(afterVimEnter, 10)
+		end
+	end
+
+	afterVimEnter()
 end
-setup()
