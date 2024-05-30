544469c73a7708a12a9853d74485626809beaa2c
yusufshalaby <yusufshalaby@gmail.com>
2024-05-30 00:58:12 -0400

    add scala metals lsp support

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -40,6 +40,29 @@ return {
 		end,
 	},
 	{
+		"scalameta/nvim-metals",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		ft = { "scala", "sbt", "java" },
+		opts = function()
+			local metals_config = require("metals").bare_config()
+			metals_config.on_attach = require("plugins.lspconfig").on_attach
+			metals_config.capabilities = require("plugins.lspconfig").capabilities
+			return metals_config
+		end,
+		config = function(self, metals_config)
+			local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = self.ft,
+				callback = function()
+					require("metals").initialize_or_attach(metals_config)
+				end,
+				group = nvim_metals_group,
+			})
+		end,
+	},
+	{
 		"nvim-neotest/neotest",
 		dependencies = {
 			require("plugins.neotest").dependencies,
