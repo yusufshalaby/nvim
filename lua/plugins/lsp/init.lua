b5b03dab199ab3aacda330ef48caaedacd00a341
yusufshalaby <yusufshalaby@gmail.com>
2024-10-19 13:43:44 -0400

    replace neodev with lazydev

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,5 +1,6 @@
 return {
 	require("plugins.lsp.lspconfig"),
+	require("plugins.lsp.lazydev"),
 	require("plugins.lsp.rustacean"),
 	require("plugins.lsp.nvim-metals"),
 }
--- lua/plugins/lsp/lspconfig/init.lua
+++ lua/plugins/lsp/lspconfig/init.lua
@@ -5,11 +5,9 @@ return {
 		{ "williamboman/mason.nvim", opts = { ui = { border = "rounded" } } },
 		"williamboman/mason-lspconfig.nvim",
 		{ "j-hui/fidget.nvim", opts = {} },
-		"folke/neodev.nvim",
 	},
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
-		require("neodev").setup()
 		require("plugins.lsp.lspconfig.mason")
 		require("plugins.lsp.lspconfig.languages.init")
 	end,
