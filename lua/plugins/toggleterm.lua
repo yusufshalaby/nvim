f53b019e09c285b35c2a4c14047f6b47808edf86
yusufshalaby <yusufshalaby@gmail.com>
2024-10-23 12:32:57 -0400

    introduce toggleterm plugin

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,22 @@
+return {
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	config = true,
+	keys = {
+		{
+			"<C-\\><C-\\>",
+			"<CMD>ToggleTerm<CR>",
+			{ desc = "Toggle terminal" },
+		},
+		{
+			"<C-\\>v",
+			"<CMD>ToggleTerm direction=vertical size=40<CR>",
+			{ desc = "Toggle terminal vertical" },
+		},
+		{
+			"<C-\\>s",
+			"<CMD>ToggleTerm direction=horizontal<CR>",
+			{ desc = "Toggle terminal horizontal" },
+		},
+	},
+}
