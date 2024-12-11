6e077adaca4ae29f856c963dad0df6bf91239c4c
yusufshalaby <yusufshalaby@gmail.com>
2024-12-11 09:41:19 -0500

    exit kitty scrollback buffer with i or a like entering insert mode

--- lua/kitty+page.lua
+++ lua/kitty+page.lua
@@ -25,6 +25,8 @@ return function(INPUT_LINE_NUMBER, CURSOR_LINE, CURSOR_COLUMN)
 	local term_buf = vim.api.nvim_create_buf(true, false)
 	local term_io = vim.api.nvim_open_term(term_buf, {})
 	vim.api.nvim_buf_set_keymap(term_buf, "n", "q", "<Cmd>q<CR>", {})
+	vim.api.nvim_buf_set_keymap(term_buf, "n", "i", "<Cmd>q<CR>", {})
+	vim.api.nvim_buf_set_keymap(term_buf, "n", "a", "<Cmd>q<CR>", {})
 	local group = vim.api.nvim_create_augroup("kitty+page", {})
 
 	local setCursor = function()
