75875c85ba19338d7ba0943b834d680f0dfe45f2
yusufshalaby <yusufshalaby@gmail.com>
2025-03-31 16:55:29 -0400

    use blink release tag instead of nightly

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -6,9 +6,9 @@ return {
 	dependencies = "rafamadriz/friendly-snippets",
 
 	-- use a release tag to download pre-built binaries
-	-- version = "v0.*",
+	version = "v1.*",
 	-- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-	build = "cargo +nightly build --release",
+	-- build = "cargo +nightly build --release",
 	-- If you use nix, you can build from source using latest nightly rust with:
 	-- build = 'nix run .#build-plugin',
 
@@ -59,6 +59,9 @@ return {
 			-- optionally disable cmdline completions
 			-- cmdline = {},
 		},
+		-- cmdline = {
+		-- 	enabled = true,
+		-- },
 
 		-- experimental signature help support
 		signature = {
