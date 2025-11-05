337fa60a2a320e8e6c196f6043677c38d5fbdf6d
yusufshalaby <yusufshalaby@honeycomb.io>
2025-11-05 09:43:19 -0500

    fix sqlfluff not reespecting .sqlfluff dialect issue

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -13,12 +13,13 @@ return {
 
 		-- Conditionally enable sqlfluff if it's in PATH
 		if vim.fn.executable("sqlfluff") == 1 then
+			require("lint").linters.sqlfluff.args = { "lint", "--format=json" }
 			lint.linters_by_ft.sql = { "sqlfluff" }
 		end
 
 		vim.api.nvim_create_augroup("lint", { clear = true })
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
-			group="lint",
+			group = "lint",
 			callback = function()
 				local ft = vim.bo.filetype
 				-- Only try linting if there is a linter configured for the current filetype
